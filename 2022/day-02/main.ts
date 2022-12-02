10ab8f731785ff5f57953347d3d9a74f9167613b
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-02 16:43:31 +0100

    feat(day-02): Solve the second challenge

--- 2022/day-02/main.ts
+++ 2022/day-02/main.ts
@@ -6,17 +6,41 @@ const RoundResult = {
   Win: 6,
 } as const;
 
+enum EnemyOptions {
+  Rock = "A",
+  Paper = "B",
+  Scissors = "C",
+}
+
 enum MyOptions {
   Rock = "X",
   Paper = "Y",
   Scissors = "Z",
 }
 
-enum EnemyOptions {
-  Rock = "A",
-  Paper = "B",
-  Scissors = "C",
+const EnemyOptionsDictionary = {
+  [EnemyOptions.Rock]: MyOptions.Rock,
+  [EnemyOptions.Paper]: MyOptions.Paper,
+  [EnemyOptions.Scissors]: MyOptions.Scissors,
+} as const;
+
+enum DesiredOutcome {
+  Loss = "X",
+  Draw = "Y",
+  Win = "Z",
 }
+
+const WinGuide = {
+  [EnemyOptions.Rock]: MyOptions.Paper,
+  [EnemyOptions.Paper]: MyOptions.Scissors,
+  [EnemyOptions.Scissors]: MyOptions.Rock,
+} as const;
+
+const LossGuide = {
+  [EnemyOptions.Paper]: MyOptions.Rock,
+  [EnemyOptions.Scissors]: MyOptions.Paper,
+  [EnemyOptions.Rock]: MyOptions.Scissors,
+} as const;
 
 const BattleOptions = {
   [MyOptions.Rock]: 1,
@@ -29,7 +53,7 @@ type Battle = [EnemyOptions, MyOptions];
 type Input = Battle[];
 
 function parseInput(content: string): Input {
-  const rawBattles = content.split(JUMP_LINE);
+  const rawBattles = content.split(JUMP_LINE).filter(Boolean);
 
   return rawBattles.map(
     (rawBattle) => rawBattle.split(SPACE).map((option) => option) as Battle
@@ -84,11 +108,37 @@ function getResults(rounds: Input): number {
     .reduce((prev, acc) => prev + acc, 0);
 }
 
+function withDesiredOutcome(rounds: Input): Input {
+  return rounds.map(([elf, outcome]) => {
+    let mine = "";
+
+    switch (outcome as string as DesiredOutcome) {
+      case DesiredOutcome.Draw:
+        mine = EnemyOptionsDictionary[elf];
+        break;
+
+      case DesiredOutcome.Win:
+        mine = WinGuide[elf];
+        break;
+
+      case DesiredOutcome.Loss:
+        mine = LossGuide[elf];
+        break;
+    }
+
+    return [elf, mine as MyOptions];
+  });
+}
+
 function main({ star, day, type }: MainProps) {
   const content = readInput({ star, day, type });
-  const parsed = parseInput(content);
+  let rounds = parseInput(content);
 
-  const points = getResults(parsed);
+  if (star === "second") {
+    rounds = withDesiredOutcome(rounds);
+  }
+
+  const points = getResults(rounds);
 
   switch (star) {
     case "first":
@@ -100,6 +150,6 @@ function main({ star, day, type }: MainProps) {
 
 // entrypoint
 (() => {
-  const result = main({ star: "first", day: 2, type: "test" });
+  const result = main({ star: "second", day: 2, type: "test" });
   console.log({ result });
 })();

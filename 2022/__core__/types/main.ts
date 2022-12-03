96a906ce1e1976ca49c5b09b9dea20a1cf6cdf59
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-03 11:53:50 +0100

    docs: Add TSDoc for the MainProps

--- 2022/__core__/types/main.ts
+++ 2022/__core__/types/main.ts
@@ -1,7 +1,19 @@
 export type AdventOfCodeStar = "first" | "second";
+export type AdventOfCodeType = "example" | "test";
 
 export type MainProps = {
+  /**
+   * Identify the number of the challenge you're currently working on
+   */
   day: number;
+  /**
+   * Use `first` for the first part of the challenge.\
+   * Use `second` for the second part of the challenge
+   */
   star: AdventOfCodeStar;
-  type: "example" | "test";
+  /**
+   * Use `example` for the given input with an explanation.\
+   * Use `test` for your real puzzle input that must be generated
+   */
+  type: AdventOfCodeType;
 };

cd037c07c93a97303f6d64d750028992e0de5352
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-04 14:24:38 +0100

    feat(2021): Integrate base cli validation

--- 2021/core/cli.go
+++ 2021/core/cli.go
@@ -9,15 +9,37 @@ import (
 func Cli() {
 	if len(os.Args) < 3 {
 		panic("Not enough arguments provided, \"go run main.go [run|create] [day]\"")
+	} else if len(os.Args) < 3 {
+		panic("Too many arguments provided, \"go run main.go [run|create] [day]\"")
 	}
 
-	if !Contains([]string{"run", "create"}, os.Args[1]) {
+	var action = os.Args[1]
+
+	if !Contains([]string{"run", "create"}, action) {
 		panic("The provided action does not sastisfy the requirements, [run|create]")
 	}
 
-	if _, err := strconv.Atoi(os.Args[2]); err != nil {
+	var day = os.Args[2]
+	parsedDay, err := strconv.Atoi(day)
+
+	if err != nil {
 		panic("The given day was not detected as a number")
 	}
 
-	fmt.Println(os.Args[1])
+	switch action {
+	case "run":
+		runDay(parsedDay)
+		return
+	case "create":
+		createDay(parsedDay)
+		return
+	}
+}
+
+func createDay(day int) {
+	fmt.Println("Attempting to create day", day)
+}
+
+func runDay(day int) {
+	fmt.Println("Attempting to run day", day)
 }

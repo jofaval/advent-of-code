308dd9eb4e79e111df45c890c67f3ced9988c9a2
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-07 18:47:04 +0100

    feat(2021): Integrate mem usage benchmarks

--- 2021/core/system.go
+++ 2021/core/system.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"fmt"
+	"runtime"
+
 	"github.com/otiai10/copy"
 )
 
@@ -8,4 +11,30 @@ import (
 func CopyFolder(source string, destination string) bool {
 	err := copy.Copy(source, destination)
 	return err == nil
+}
+
+// bytes to kilo-bytes
+func bToKb(b uint64) uint64 {
+	return b / 1024
+}
+
+type AllocStats struct {
+	Allocated      uint64
+	TotalAllocated uint64
+}
+
+// Source: https://gist.github.com/j33ty/79e8b736141be19687f565ea4c6f4226
+func GetMemoryUsage() AllocStats {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	return AllocStats{
+		Allocated:      bToKb(m.Alloc),
+		TotalAllocated: bToKb(m.TotalAlloc),
+	}
+}
+
+func DisplayMemoryUsage(usage AllocStats) {
+	fmt.Println("Allocated =", usage.Allocated, "KiB\t", "Total Allocated =", usage.TotalAllocated, "KiB")
 }
--- 2021/main.go
+++ 2021/main.go
@@ -5,12 +5,20 @@ import (
 	"time"
 
 	"jofaval.advent-of-code/2021/cli"
+	"jofaval.advent-of-code/2021/core"
 )
 
 func main() {
+	memoryUsageStart := core.GetMemoryUsage()
 	start := time.Now()
 
 	cli.Cli()
 
 	fmt.Println(time.Since(start))
+
+	memoryUsageEnd := core.GetMemoryUsage()
+	core.DisplayMemoryUsage(core.AllocStats{
+		TotalAllocated: memoryUsageEnd.TotalAllocated - memoryUsageStart.TotalAllocated,
+		Allocated:      memoryUsageEnd.Allocated - memoryUsageStart.Allocated,
+	})
 }

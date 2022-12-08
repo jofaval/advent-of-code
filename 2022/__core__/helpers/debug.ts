13e88cc88c3ff87a63f00cf3f119a9b959e50747
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-08 16:02:28 +0100

    feat(2022): Integrate memory usage utility

--- 2022/__core__/helpers/debug.ts
+++ 2022/__core__/helpers/debug.ts
@@ -11,3 +11,26 @@ export function logDebug(...params: any[]) {
 
   console.log(...params);
 }
+
+/**
+ * @source https://stackoverflow.com/questions/20018588/how-to-monitor-the-memory-usage-of-node-js#answer-64550489
+ */
+export const displayMemoryUsage = () => {
+  const formatMemoryUsage = (data) =>
+    `${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
+
+  const memoryData = process.memoryUsage();
+
+  const memoryUsage = {
+    /** Resident Set Size - total memory allocated for the process execution */
+    rss: `${formatMemoryUsage(memoryData.rss)}`,
+    /** total size of the allocated heap */
+    heapTotal: `${formatMemoryUsage(memoryData.heapTotal)}`,
+    /** actual memory used during the execution */
+    heapUsed: `${formatMemoryUsage(memoryData.heapUsed)}`,
+    /** V8 external memory */
+    external: `${formatMemoryUsage(memoryData.external)}`,
+  };
+
+  console.log(memoryUsage);
+};

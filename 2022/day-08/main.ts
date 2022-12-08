c0714c29e654f4a3e4e6e34fe5388b2c92234b21
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-08 08:01:31 +0100

    feat(day-08): Solve the second challenge

--- 2022/day-08/main.ts
+++ 2022/day-08/main.ts
@@ -1,11 +1,4 @@
-import {
-  DOUBLE_JUMP_LINE,
-  JUMP_LINE,
-  logDebug,
-  MainProps,
-  readInput,
-  setDebug,
-} from "../__core__";
+import { JUMP_LINE, MainProps, readInput } from "../__core__";
 
 type TreeHeight = number;
 
@@ -50,7 +43,7 @@ function toEdge(
       }
       return elements;
     case Direction.Left:
-      return grid[x].slice(0, y);
+      return grid[x].slice(0, y).reverse();
   }
 }
 
@@ -58,7 +51,13 @@ function isHigher(current: number, rest: number[]): boolean {
   return rest.every((tree) => tree < current);
 }
 
-function isVisible(grid: Input, row: number, col: number): boolean {
+type CoordinatesVisibility = [boolean, boolean, boolean, boolean];
+
+function isVisible(
+  grid: Input,
+  row: number,
+  col: number
+): CoordinatesVisibility {
   const current = grid[row][col];
 
   const topVisible = isHigher(current, toEdge(grid, row, col, Direction.Top));
@@ -72,56 +71,111 @@ function isVisible(grid: Input, row: number, col: number): boolean {
   );
   const leftVisible = isHigher(current, toEdge(grid, row, col, Direction.Left));
 
-  return topVisible || rightVisible || bottomVisible || leftVisible;
+  return [topVisible, rightVisible, bottomVisible, leftVisible];
+}
+
+function getGridSize(grid: Input): { width: number; height: number } {
+  return {
+    width: grid.length,
+    height: grid[0].length,
+  };
+}
+
+function mapGrid(
+  grid: Input,
+  callback: (visibility: CoordinatesVisibility, x: number, y: number) => void
+) {
+  const { width, height } = getGridSize(grid);
+
+  for (let x = 1; x < width - 1; x++) {
+    for (let y = 1; y < height - 1; y++) {
+      const visible = isVisible(grid, x, y);
+      callback(visible, x, y);
+    }
+  }
 }
 
 function countVisibleTrees(grid: Input): number {
-  const width = grid.length;
-  const height = grid[0].length;
+  const { width, height } = getGridSize(grid);
   // minus four because of the collision corners
   let visibleTrees = Math.round(width * 2 + (height * 2 - 4));
 
-  const visibilityGrid = [];
-
-  for (let x = 1; x < width - 1; x++) {
-    const row = [];
-    for (let y = 1; y < height - 1; y++) {
-      const visible = isVisible(grid, x, y);
-      if (visible) {
-        visibleTrees++;
-      }
-      row.push(visible);
+  mapGrid(grid, (visibility) => {
+    if (visibility.some(Boolean)) {
+      visibleTrees++;
     }
-    visibilityGrid.push(row);
-  }
-
-  logDebug(
-    visibilityGrid
-      .map((row) => row.map((cell) => cell.toString().padEnd(5, " ")).join(" "))
-      .join(DOUBLE_JUMP_LINE)
-  );
+  });
 
   return visibleTrees;
 }
 
-function main({ star, day, type }: MainProps) {
-  setDebug(type === "example");
+function nonBlockingTreesScore(current: number, view: number[]): number {
+  let nonBlocking = 0;
 
+  for (let index = 0; index < view.length; index++) {
+    nonBlocking++;
+
+    if (current <= view[index]) {
+      break;
+    }
+  }
+
+  return Math.max(1, nonBlocking);
+}
+
+function getScenicScore(grid: Input, row: number, col: number): number {
+  const current = grid[row][col];
+
+  const top = nonBlockingTreesScore(
+    current,
+    toEdge(grid, row, col, Direction.Top)
+  );
+  const right = nonBlockingTreesScore(
+    current,
+    toEdge(grid, row, col, Direction.Right)
+  );
+  const bottom = nonBlockingTreesScore(
+    current,
+    toEdge(grid, row, col, Direction.Bottom)
+  );
+  const left = nonBlockingTreesScore(
+    current,
+    toEdge(grid, row, col, Direction.Left)
+  );
+
+  return top * right * bottom * left;
+}
+
+function getMostScenicTree(grid: Input): number {
+  let mostScenicScore = -Infinity;
+
+  mapGrid(grid, (visibility, x, y) => {
+    if (visibility.some(Boolean)) {
+      const currentScore = getScenicScore(grid, x, y);
+
+      if (currentScore > mostScenicScore) {
+        mostScenicScore = currentScore;
+      }
+    }
+  });
+
+  return mostScenicScore;
+}
+
+function main({ star, day, type }: MainProps) {
   const content = readInput({ star, day, type });
   const grid = parseInput(content);
 
-  const totalVisibleTrees = countVisibleTrees(grid);
-
   switch (star) {
     case "first":
-      return totalVisibleTrees;
+      return countVisibleTrees(grid);
     case "second":
-      return totalVisibleTrees;
+      return getMostScenicTree(grid);
   }
 }
 
 // entrypoint
 (() => {
-  const result = main({ star: "first", day: 8, type: "test" });
+  const result = main({ star: "second", day: 8, type: "test" });
   console.log({ result });
 })();

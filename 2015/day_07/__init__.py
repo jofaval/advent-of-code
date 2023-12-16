f3f09796e8c488b6823ceba8bcee5d1f38029f6f
pepe_fabra_valverde <jofaval@outlook.com>
2023-12-16 23:58:58 +0100

    feat(2015): day 07 more progress

--- 2015/day_07/__init__.py
+++ 2015/day_07/__init__.py
@@ -4,6 +4,7 @@ https://adventofcode.com/2015/day/7
 """
 
 import re
+from typing import List
 
 # core
 from ..core import (AdventOfCodeChallenge, Input, Star, benchmark, read,
@@ -17,20 +18,88 @@ def is_numeric(value: str) -> bool:
     return re.search(r"\d+", value) is not None
 
 
+def bitwise_uint16_and(first, second):
+    """bitwise uint16 AND implementation"""
+    return [
+        int(first_value == 1 and second[i] == 1)
+        for i, first_value in enumerate(first)
+    ]
+
+
+def bitwise_uint16_lshift(first, second: int):
+    """inefficient bitwise uint16 LSHIFT implementation"""
+    result = [*first]
+
+    for _ in range(0, second):
+        result = [*result[1:], 0]
+
+    return result
+
+
+def bitwise_uint16_rshift(first, second: int):
+    """inefficient bitwise uint16 RSHIFT implementation"""
+    result = [*first]
+
+    for _ in range(0, second):
+        result.pop()
+        result = [0, *result[:-1]]
+
+    return result
+
+
+def bitwise_uint16_not(first):
+    """bitwise uint16 NOT implementation"""
+    return [1 if value == 0 else 0 for value in first]
+
+
+def bitwise_uint16_or(first, second):
+    """bitwise uint16 OR implementation"""
+    return [
+        int(first_value == 1 or second[i] == 1)
+        for i, first_value in enumerate(first)
+    ]
+
+
 def evaluate_operation(operation: str, first: int, second: int = None):
     """bitwise operations"""
     if DEBUG:
         print({"first": first, "second": second})
     if operation == "AND":
-        return first & second
+        return bitwise_uint16_and(first, second)
     elif operation == "LSHIFT":
-        return first << second
+        return bitwise_uint16_lshift(first, second)
     elif operation == "RSHIFT":
-        return first >> second
+        return bitwise_uint16_rshift(first, second)
     elif operation == "NOT":
-        return ~(first << 16)
+        return bitwise_uint16_not(first)
     elif operation == "OR":
-        return first | second
+        return bitwise_uint16_or(first, second)
+
+
+def to_uint16(number: int):
+    """inefficient Python's uint16 implementation"""
+    total = 0
+    accumulative = []
+    for i in range(0, 16):
+        current = 2 ** (15 - i)
+        if total + current <= number:
+            accumulative.append(1)
+            total += current
+        else:
+            accumulative.append(0)
+
+    return accumulative
+
+
+def from_uint16(uint16: List[int]):
+    total = 0
+
+    for i, n in enumerate(uint16):
+        if n == 0:
+            continue
+        total += 2 ** (15 - i)
+
+    return total
 
 
 def safe_evaluation(stash: dict, operation: str, first: int, second: int = None):
@@ -70,7 +139,7 @@ def main() -> None:
     """Main flow of execution"""
     challenge = AdventOfCodeChallenge(
         day=7,
-        input=Input.TEST,
+        input=Input.PROD,
         star=Star.FIRST
     )
 
@@ -79,33 +148,41 @@ def main() -> None:
     result = None
     stash = dict()
 
-    if challenge['star'] == Star.FIRST:
-        for line in content.split("\n"):
-            operation, address = line.split(POINTER_SEPARATOR)
-            if re.search(r"(\w+|\d+)\s\w+\s(\w+|\d+)", operation) is not None:
-                first, action, second = operation.split(" ")
-                if DEBUG:
-                    print(first, action, second)
-                stash[address] = safe_evaluation(stash, action, first, second)
-            elif re.search(r"\w+\s(\w+|\d+)", operation) is not None:
-                action, first = operation.split(" ")
-                if DEBUG:
-                    print(first, action)
-                stash[address] = safe_evaluation(stash, action, first)
-            else:
-                value = operation
-                stash[address] = int(value)
+    try:
+        if challenge['star'] == Star.FIRST:
+            for line in content.split("\n"):
+                operation, address = line.split(POINTER_SEPARATOR)
+                if re.search(r"(\w+|\d+)\s\w+\s(\w+|\d+)", operation) is not None:
+                    first, action, second = operation.split(" ")
+                    if DEBUG:
+                        print(first, action, second)
+                    stash[address] = safe_evaluation(
+                        stash, action, first, second)
+                elif re.search(r"\w+\s(\w+|\d+)", operation) is not None:
+                    action, first = operation.split(" ")
+                    if DEBUG:
+                        print(first, action)
+                    stash[address] = safe_evaluation(stash, action, first)
+                else:
+                    value = operation
+                    stash[address] = to_uint16(int(value))
 
-            if DEBUG:
-                print(line)
-                print(stash)
-                print("")
-    elif challenge['star'] == Star.SECOND:
-        raise Exception(
-            f"No result was prepared for the {Star.SECOND.value} star."
-        )
+                if DEBUG:
+                    print(line)
+                    print(stash)
+                    print("")
+        elif challenge['star'] == Star.SECOND:
+            raise Exception(
+                f"No result was prepared for the {Star.SECOND.value} star."
+            )
+    except Exception as error:
+        print(error)
 
-    return stash
+    parsed_stash = {
+        key: from_uint16(value)
+        for key, value in stash.items()
+    }
+    return parsed_stash
 
 
 if __name__ == "__main__":

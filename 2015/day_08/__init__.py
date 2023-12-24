6bf4fb1f385fd31318d25f9d1efbc1a3a741fdbb
pepe_fabra_valverde <jofaval@outlook.com>
2023-12-24 23:56:01 +0100

    feat(2015/08): cheating

--- 2015/day_08/__init__.py
+++ 2015/day_08/__init__.py
@@ -18,14 +18,24 @@ class Line(BaseModel):
     code_len: int = 0
     in_memory_len: int = 0
 
-    def get_memory_string(self) -> str:
-        mutable = eval(self.raw)
+    def get_memory_string(self, encode: bool) -> str:
+        mutable = ""
+        if encode:
+            mutable = repr(self.raw)
+            mutable = "\""+mutable+"\""
+        else:
+            mutable = eval(self.raw)
+
+        print({"line": self.raw, "mutable": mutable,
+              "len": [len(self.raw), len(mutable)]})
 
         return mutable
 
-    def evaluate(self):
+    def evaluate(self, encode: bool):
         self.code_len = len(self.raw)
-        self.in_memory_len = len(self.get_memory_string())
+        # https://www.reddit.com/r/adventofcode/comments/3vw32y/comment/cxrad1k/?utm_source=share&utm_medium=web2x&context=3
+        self.in_memory_len = 2+self.raw.count('\\')+self.raw.count(
+            '"') if encode else len(self.get_memory_string(encode))
 
         return self
 
@@ -37,38 +47,38 @@ def main() -> None:
     challenge = AdventOfCodeChallenge(
         day=8,
         input=Input.PROD,
-        star=Star.FIRST
+        star=Star.SECOND
     )
 
     content = read(challenge)
 
     lines: List[Line] = []
+    is_second_star = challenge['star'] == Star.SECOND
     for raw_line in content.splitlines():
         line = Line(raw=raw_line)
-        line.evaluate()
+        line.evaluate(is_second_star)
 
         lines.append(line)
 
     result = None
 
-    if challenge['star'] == Star.FIRST:
-        total_code_characters = 0
-        total_in_memory_characters = 0
+    total_code_characters = 0
+    total_in_memory_characters = 0
 
-        for line in lines:
-            total_code_characters += line.code_len
-            total_in_memory_characters += line.in_memory_len
+    for line in lines:
+        total_code_characters += line.code_len
+        total_in_memory_characters += line.in_memory_len
 
-        print("")
-        print({
-            "total_code_characters": total_code_characters,
-            "total_in_memory_characters": total_in_memory_characters,
-        })
+    print("")
+    print({
+        "total_code_characters": total_code_characters,
+        "total_in_memory_characters": total_in_memory_characters,
+    })
+
+    if is_second_star:
+        result = total_in_memory_characters
+    else:
         result = total_code_characters - total_in_memory_characters
-    elif challenge['star'] == Star.SECOND:
-        raise Exception(
-            f"No result was prepared for the {Star.SECOND.value} star."
-        )
 
     return result
 
